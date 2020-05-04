8bc13087712aedaa7b18a13cea3c25dd8a7c72a7
王磊 <wanglei1@e6yun.com>
2020-05-04 11:41:21 +0800

    添加项目swagger文档

---
+++ fhs_module/fhs_module_base/src/main/java/com/fhs/module/base/swagger/controller/SwaggerRedirectController.java
@@ -0,0 +1,29 @@
+package com.fhs.module.base.swagger.controller;
+
+import com.fhs.core.base.controller.BaseController;
+import com.fhs.core.config.EConfig;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.io.IOException;
+
+/**
+ * swagger转发控制器
+ */
+@Controller
+@RequestMapping("ms/swagger")
+public class SwaggerRedirectController extends BaseController {
+
+    @Value("${fhs.swagger.enable:false}")
+    private boolean isEnable;
+
+    @RequestMapping("302")
+    public void redirect() throws IOException {
+        if(isEnable){
+            super.getResponse().sendRedirect(EConfig.getPathPropertiesValue("basePath") + "/doc.html");
+            return;
+        }
+        super.outWrite("swagger未开启,请配置:fhs.swagger.enable,将其设置为true");
+    }
+}
