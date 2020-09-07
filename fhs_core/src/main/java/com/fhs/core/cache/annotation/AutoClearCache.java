c7958ff2506841ddc827d81f2160ef7fce7573f0
wanglei <wanglei1@e6yun.com>
2020-09-07 16:30:12 +0800

    添加cache自动刷新注解

---
+++ fhs_core/src/main/java/com/fhs/core/cache/annotation/AutoClearCache.java
@@ -0,0 +1,21 @@
+package com.fhs.core.cache.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * fhs framework 对于 jetcahce的扩展注解
+ * 用于namespaces  的数据改变之后 自动清除缓存
+ * by jackwong
+ * @author jackwong
+ * @date 2020-05-19 16:15:18
+ */
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+@Target({ElementType.METHOD, ElementType.FIELD,ElementType.TYPE,})
+public @interface AutoClearCache {
+    /**
+     * 需要自动清除缓存的命名空间
+     * @return
+     */
+    String[] namespaces() default {};
+}
