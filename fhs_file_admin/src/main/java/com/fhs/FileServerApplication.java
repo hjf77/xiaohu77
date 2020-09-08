6620422b6b4526c755fa47af58a6d93cb280a19e
wanglei <wanglei1@e6yun.com>
2020-09-08 19:11:06 +0800

    解决报错问题

--- fhs_file_admin/src/main/java/com/fhs/FileServerApplication.java
+++ fhs_file_admin/src/main/java/com/fhs/FileServerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * 文件服务
