6e4d8ce90b03e496a5c03e317095b860428e4219
zhangyao <18892033749@qq.com>
2020-11-30 16:43:34 +0800

    解决成jar包后路径问题

--- fhs_module/fhs_bis_logger/fhs_bis_logger_service/src/main/java/com/fhs/bislogger/service/impl/LogLoginServiceImpl.java
+++ fhs_module/fhs_bis_logger/fhs_bis_logger_service/src/main/java/com/fhs/bislogger/service/impl/LogLoginServiceImpl.java
@@ -28,6 +28,7 @@ import java.lang.reflect.Method;
 import java.util.Date;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 /**
  * 登录日志(LogLogin)表服务实现类
@@ -57,54 +58,7 @@ public class LogLoginServiceImpl extends BaseServiceImpl<LogLoginVO, LogLoginDO>
             if ("0:0:0:0:0:0:0:1".equals(ip) || "172.".equals(ip.substring(0, 4)) || "192.".equals(ip.substring(0, 4))) {
 
             } else {
-                //db
-                String dbPath = LogLoginServiceImpl.class.getResource("/data/ip2region.db").getPath();
-                System.out.println(dbPath);
-
-                File file = new File(dbPath);
-                if (file.exists() == false) {
-
-                    String tmpDir = System.getProperties().getProperty("java.io.tmpdir");
-                    dbPath = tmpDir + "ip.db";
-                    System.out.println(dbPath);
-                    file = new File(dbPath);
-                    FileUtils.copyInputStreamToFile(LogLoginServiceImpl.class.getClassLoader().getResourceAsStream("classpath:data/ip2region.db"), file);
-
-                }
-                //查询算法
-                int algorithm = DbSearcher.BTREE_ALGORITHM; //B-tree
-                //DbSearcher.BINARY_ALGORITHM //Binary
-                //DbSearcher.MEMORY_ALGORITYM //Memory
-                try {
-                    DbConfig config = new DbConfig();
-                    DbSearcher searcher = new DbSearcher(config, dbPath);
-
-                    //define the method
-                    Method method = null;
-                    switch (algorithm) {
-                        case DbSearcher.BTREE_ALGORITHM:
-                            method = searcher.getClass().getMethod("btreeSearch", String.class);
-                            break;
-                        case DbSearcher.BINARY_ALGORITHM:
-                            method = searcher.getClass().getMethod("binarySearch", String.class);
-                            break;
-                        case DbSearcher.MEMORY_ALGORITYM:
-                            method = searcher.getClass().getMethod("memorySearch", String.class);
-                            break;
-                    }
-
-                    DataBlock dataBlock = null;
-                    if (Util.isIpAddress(ip) == false) {
-                        log.error("Error: Invalid ip address");
-                    }
-
-                    dataBlock = (DataBlock) method.invoke(searcher, ip);
-
-                    addresses = dataBlock.getRegion();
-
-                } catch (Exception e) {
-                    log.error("通过ip2region获取当前登录人信息失败 : ", e);
-                }
+                addresses = getCityInfo(ip);
 //                addresses = getLoginUserMsgUtil.getAddresses( ip);
             }
         } catch (IOException e) {
@@ -138,5 +92,46 @@ public class LogLoginServiceImpl extends BaseServiceImpl<LogLoginVO, LogLoginDO>
     @Override
     public List<LogLoginVO> getLoginIogSummary(Date statTime, Date endTime) {
         return logLoginMapper.getLoginIogSummary(statTime, endTime);
+    }
+
+    /**
+     * 获取IP地址所在城市
+     *
+     * @param ip
+     * @return
+     */
+    private String getCityInfo(String ip) {
+        DbSearcher searcher = null;
+        try {
+            String dbPath = LogLoginServiceImpl.class.getResource("/data/ip2region.db").getPath();
+            File file = new File(dbPath);
+            if (!file.exists()) {
+                String tmpDir = System.getProperties().getProperty("java.io.tmpdir");
+                dbPath = tmpDir + "ip.db";
+                file = new File(dbPath);
+                FileUtils.copyInputStreamToFile(Objects.requireNonNull(LogLoginServiceImpl.class.getClassLoader().getResourceAsStream("classpath:data/ip2region.db")), file);
+            }
+            int algorithm = DbSearcher.BTREE_ALGORITHM;
+            DbConfig config = new DbConfig();
+            searcher = new DbSearcher(config, file.getPath());
+            Method method = searcher.getClass().getMethod("btreeSearch", String.class);
+            DataBlock dataBlock = null;
+            if (!Util.isIpAddress(ip)) {
+                log.error("Error: Invalid ip address");
+            }
+            dataBlock = (DataBlock) method.invoke(searcher, ip);
+            return dataBlock.getRegion();
+        } catch (Exception e) {
+            log.error("获取地址信息异常", e);
+        } finally {
+            if (searcher != null) {
+                try {
+                    searcher.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return "";
     }
 }
