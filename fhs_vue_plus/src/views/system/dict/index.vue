545ea8f6d19b714f75d137b8bdda1abe6d330d4a
zhangyingke <2806703242@qq.com>
2021-04-06 09:50:31 +0800

    字典页面修改

--- fhs_vue_plus/src/views/system/dict/index.vue
+++ fhs_vue_plus/src/views/system/dict/index.vue
@@ -1,5 +1,74 @@
 <template>
   <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" :inline="true" label-width="68px">
+      <el-form-item  prop="groupName">
+        <el-input
+          v-model="queryParams.groupName"
+          placeholder="分组名称"
+          clearable
+          size="small"
+          style="width: 240px"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+      <el-form-item  prop="wordbookGroupCode">
+        <el-input
+          v-model="queryParams.wordbookGroupCode"
+          placeholder="分组code"
+          clearable
+          size="small"
+          style="width: 240px"
+          @keyup.enter.native="handleQuery"
+        />
+      </el-form-item>
+
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['wordbook:add']"
+        >新增</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['wordbook:update']"
+        >修改</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          icon="el-icon-delete"
+          size="mini"
+          :disabled="multiple"
+          @click="handleDelete"
+          v-hasPermi="['wordbook:del']"
+        >删除</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="danger"
+          icon="el-icon-edit"
+          size="mini"
+          @click="refreshCache"
+          v-hasPermi="['wordbook:refreshRedisCache']"
+        >刷新所有字典缓存</el-button>
+      </el-col>
+
+    </el-row>
     <crud
       :filter="filter"
       :columns="columns"
@@ -45,8 +114,8 @@ export default {
       ],
       filter:{
         controls:[
-          {label:'分组名称',name:'groupName',placeholder: "通过关键字搜索",type:'text'},
-          {label:'分组编码',name:'wordbookGroupCode',placeholder: "通过关键字搜索",type:'text'}
+          {name:'groupName',placeholder: "分组名称",type:'text'},
+          {name:'wordbookGroupCode',placeholder: "分组code",type:'text'}
         ]
       },
 
@@ -89,6 +158,139 @@ export default {
   },
   created() {
   },
-  methods: {}
+  methods: {
+    /** 查询字典类型列表 */
+    getList() {
+      this.loading = true;
+      listType(this.addDateRange(this.queryParams, this.dateRange)).then(response => {
+          this.typeList = response.rows;
+          this.total = response.total;
+          this.loading = false;
+        }
+      );
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        groupId: undefined,
+        groupName: undefined,
+        wordbookGroupCode: undefined,
+
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.dateRange = [];
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加字典类型";
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.dictId)
+      this.single = selection.length!=1
+      this.multiple = !selection.length
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      this.open = true;
+      const groupId = row.groupId
+      getType(groupId).then(response => {
+        this.form = response;
+        this.title = "修改字典类型";
+      });
+    },
+    /** 提交按钮 */
+    submitForm: function() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.groupId != undefined) {
+            updateType(this.form).then(response => {
+              if (response.code === 200) {
+                this.msgSuccess("修改成功");
+                this.open = false;
+                this.getList();
+              } else {
+                this.msgError(response.msg);
+              }
+            });
+          } else {
+            addType(this.form).then(response => {
+              if (response.code === 200) {
+                this.msgSuccess("新增成功");
+                this.open = false;
+                this.getList();
+              } else {
+                this.msgError(response.msg);
+              }
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const groupId = row.groupId;
+      this.$confirm('是否确认删除分组名称为"' + row.groupName + '"的数据项?', "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(function() {
+        return delType(row.wordbookGroupCode,groupId);
+      }).then(() => {
+        this.getList();
+        this.msgSuccess("删除成功");
+      }).catch(function() {});
+    },
+    /** 刷新字典缓存按钮操作 */
+    refreshCache(row) {
+      let wordbookGroupCode ='';
+      if (row.wordbookGroupCode != null || row.wordbookGroupCode !== undefined){
+        wordbookGroupCode = row.wordbookGroupCode;
+      }else {
+        wordbookGroupCode = '';
+      }
+      refresh(wordbookGroupCode).then(response => {
+        if (response.code === 200) {
+          this.msgSuccess("操作成功");
+          this.open = false;
+          this.getList();
+        } else {
+          this.msgError(response.msg);
+        }
+      });
+
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      const queryParams = this.queryParams;
+      this.$confirm('是否确认导出所有类型数据项?', "警告", {
+        confirmButtonText: "确定",
+        cancelButtonText: "取消",
+        type: "warning"
+      }).then(function() {
+        return exportType(queryParams);
+      }).then(response => {
+        this.download(response.msg);
+      }).catch(function() {});
+    }
+  }
 };
 </script>
