06002470574ac80db5faf590e7a6df9cfb95d148
wanglei <wanglei@xhb.com>
2021-06-02 14:55:37 +0800

    恢复误删除的类

---
+++ fhs_extends/fhs_pojo/src/main/java/com/fhs/core/base/pojo/pager/Pager.java
@@ -0,0 +1,59 @@
+package com.fhs.core.base.pojo.pager;
+
+import com.fhs.common.utils.JsonUtils;
+
+import java.util.Collection;
+
+/**
+ * 分页对象
+ * @Filename: Pager.java
+ * @Description:
+ * @Version: 1.0
+ * @Author: qixiaobo
+ * @Email: wanglei@sxpartner.com
+ * @History:<br>
+ * 陕西小伙伴网络科技有限公司
+ * Copyright (c) 2017 All Rights Reserved.
+ *
+ */
+public class Pager<T>
+{
+
+    /**
+     *一共多少条数据
+     */
+    private long total;
+
+    /**
+     *当页显示数据
+     */
+    private Collection<T> rows;
+
+
+    public Pager(long total, Collection<T> rows)
+    {
+        super();
+        this.total = total;
+        this.rows = rows;
+    }
+
+    public long getTotal()
+    {
+        return total;
+    }
+    public void setTotal(int total)
+    {
+        this.total = total;
+    }
+    public Collection<T> getRows()
+    {
+        return rows;
+    }
+    public void setRows(Collection<T> rows)
+    {
+        this.rows = rows;
+    }
+    public String asJson(){
+        return JsonUtils.bean2json(this);
+    }
+}
