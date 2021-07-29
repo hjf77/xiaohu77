b1c7e2fff0c8efbf79276c444879d651d77971e6
AnHeng <hengan@xhb.com>
2021-07-29 11:05:49 +0800

    添加注解

--- fhs_extends/fhs_excel/fhs_excel_core/src/main/java/com/fhs/core/excel/service/impl/ExcelServiceImpl.java
+++ fhs_extends/fhs_excel/fhs_excel_core/src/main/java/com/fhs/core/excel/service/impl/ExcelServiceImpl.java
@@ -28,6 +28,19 @@ import java.util.*;
 @Service
 public class ExcelServiceImpl implements ExcelService {
 
+    /**
+     * Excel导出功能
+     * 需要导出的DoClass列中必须添加ApiModelProperty和ExcelExport注解
+     * ApiModelProperty 注解用于获取字段中文描述
+     * ExcelExport 注解用于确定哪些字段需要导出
+     * 需要导出的DoClass列中选择添加Order注解
+     * Order注解用于导出字段排序，默认正序
+     *
+     * @param query 查询条件
+     * @param targetService 对应的 Service
+     * @param doClass 对应的 DO class
+     * @return Workbook Excel对象
+     */
     @Override
     public Workbook exportExcel(QueryFilter query, Class<? extends BaseService> targetService, Class<?> doClass) {
         BaseService service = SpringContextUtil.getBeanByName(targetService);
@@ -85,8 +98,24 @@ public class ExcelServiceImpl implements ExcelService {
         return wb;
     }
 
+    /**
+     * Excel导入功能
+     * 其中包括基本校验基于DO Class注解
+     * 注解 NotEmpty 非空校验
+     * 注解 Length 长度校验，包括max min
+     *
+     * 反翻译
+     * 通过Trans注解进行反翻译服务
+     *
+     * 通过ExcelUtils工具类获取datas和title
+     * @param dataArray Excel数据数组
+     * @param titleArray Excel标题数组
+     * @param targetService 对应的service
+     * @param doClass 对应的 DO Class
+     * @throws ValidationException 返回整个Excel验证结果
+     */
     @Override
-    public void importExcel(Object[][] datas, Object[] title, Class<? extends BaseService> targetService, Class<?> doClass) throws ValidationException {
+    public void importExcel(Object[][] dataArray, Object[] titleArray, Class<? extends BaseService> targetService, Class<?> doClass) throws ValidationException {
 
         BaseService service = SpringContextUtil.getBeanByName(targetService);
         List<Field> fields = ReflectUtils.getAnnotationField(doClass, ApiModelProperty.class);
@@ -95,23 +124,23 @@ public class ExcelServiceImpl implements ExcelService {
         StringBuilder valiStr = new StringBuilder();
 
         List<Object> doList = new ArrayList<>();
-        for (int i = 0; i < datas.length; i++){
+        for (int i = 0; i < dataArray.length; i++){
             doList.add(ReflectUtils.newInstance(doClass));
         }
 
-        for (int i = 0; i < title.length; i++){
+        for (int i = 0; i < titleArray.length; i++){
             for (Field field : fields){
                 String fieldName = field.getAnnotation(ApiModelProperty.class).value();
-                if (fieldName.equals(title[i])
+                if (fieldName.equals(titleArray[i])
                         && field.getAnnotation(TableField.class).exist()){
-                    for (int j = 0; j < datas.length; j ++){
+                    for (int j = 0; j < dataArray.length; j ++){
                         Object objDo = doList.get(j);
-                        Object data = datas[j][i];
+                        Object data = dataArray[j][i];
                         //反翻译
                         Trans trans = field.getAnnotation(Trans.class);
                         if (trans != null){
                             if (trans.type().equals(TransType.WORD_BOOK)){
-                                String tranStr = transService.getUnWordBookTransMap().get(trans.key() + "_" + datas[j][i]);
+                                String tranStr = transService.getUnWordBookTransMap().get(trans.key() + "_" + dataArray[j][i]);
                                 if (StringUtils.isBlank(tranStr)){
                                     valiStr.append(fieldName + "找不到对应翻译，请检查第" + (j+2) + "行“" + fieldName + "”列;\r\n");
                                 }
@@ -133,7 +162,7 @@ public class ExcelServiceImpl implements ExcelService {
                                 }
 
                             }
-                            ReflectUtils.setValue(objDo, field, datas[j][i]);
+                            ReflectUtils.setValue(objDo, field, dataArray[j][i]);
                         }
                     }
                 }
