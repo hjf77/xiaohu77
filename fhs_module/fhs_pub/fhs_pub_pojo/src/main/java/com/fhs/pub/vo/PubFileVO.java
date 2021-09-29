ef43b633454d6230ce7267324f73580453f862a6
ZhangBo <cdphantom@gmail.com>
2021-09-29 11:06:11 +0800

    附件增加相对路径filePath

--- fhs_module/fhs_pub/fhs_pub_pojo/src/main/java/com/fhs/pub/vo/PubFileVO.java
+++ fhs_module/fhs_pub/fhs_pub_pojo/src/main/java/com/fhs/pub/vo/PubFileVO.java
@@ -6,6 +6,7 @@ import io.swagger.annotations.ApiModel;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
+import org.apache.commons.lang3.StringUtils;
 
 /**
  * 公共文件
@@ -18,5 +19,14 @@ import lombok.NoArgsConstructor;
 @EqualsAndHashCode(callSuper = true)
 @ApiModel(value = "PubFileVO", description = "PubFile参数")
 public class PubFileVO extends PubFileDO implements VO {
+    // 文件相对路径, 用于nginx代理文件
+    private String filePath;
 
+    public String getFilePath() {
+        StringBuffer sb = new StringBuffer();
+        sb.append(this.getUploadDate()).append("/");
+        sb.append(StringUtils.replace(this.getFileSuffix(), ".", "")).append("/");
+        sb.append(this.getFileId()).append(this.getFileSuffix());
+        return sb.toString();
+    }
 }
