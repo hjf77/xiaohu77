3e8841f58353874e8db69bf2374ddd64820ca741
miyaxin <miyaxin0422@163.com>
2022-03-28 17:32:30 +0800

    重写字典查询接口，去掉权限判断

--- fhs_module/fhs_basics/fhs_basics_service/src/main/java/com/fhs/basics/controller/ServiceDictItemController.java
+++ fhs_module/fhs_basics/fhs_basics_service/src/main/java/com/fhs/basics/controller/ServiceDictItemController.java
@@ -1,12 +1,18 @@
 package com.fhs.basics.controller;
 
 import com.fhs.basics.po.ServiceDictItemPO;
+import com.fhs.basics.service.ServiceDictItemService;
 import com.fhs.basics.vo.ServiceDictItemVO;
 import com.fhs.basics.api.anno.LogNamespace;
 import com.fhs.module.base.controller.ModelSuperController;
 import com.fhs.module.base.swagger.anno.ApiGroup;
 import io.swagger.annotations.Api;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
 
 /**
  * 字典管理controller
@@ -21,4 +27,19 @@ import org.springframework.web.bind.annotation.*;
 @LogNamespace(namespace = "dictItem", module = "字典项管理")
 public class ServiceDictItemController extends ModelSuperController<ServiceDictItemVO, ServiceDictItemPO> {
 
+    @Autowired
+    private ServiceDictItemService serviceDictItemService;
+
+    /**
+     * 重写下拉框查询接口 去掉权限判断
+     * @param e
+     * @param request response
+     * @param response
+     * @return
+     * @throws Exception
+     */
+    @Override
+    public List<ServiceDictItemVO> findList(ServiceDictItemVO e, HttpServletRequest request, HttpServletResponse response) throws Exception {
+        return serviceDictItemService.findForList(e);
+    }
 }
