63d5c033e72374948edd6b86d5ea40595d75215c
chenzhihu <347068320@qq.com>
2022-04-06 14:38:13 +0800

    修改用户密码报错

--- fhs_module/fhs_basics/fhs_basics_api/src/main/java/com/fhs/basics/vo/UcenterMsUserVO.java
+++ fhs_module/fhs_basics/fhs_basics_api/src/main/java/com/fhs/basics/vo/UcenterMsUserVO.java
@@ -1,5 +1,6 @@
 package com.fhs.basics.vo;
 
+import com.baomidou.mybatisplus.annotation.TableField;
 import com.fhs.common.utils.StringUtils;
 import com.fhs.core.trans.vo.VO;
 import com.fhs.basics.po.UcenterMsUserPO;
@@ -44,34 +45,6 @@ public class UcenterMsUserVO extends UcenterMsUserPO implements VO {
 
     @ApiModelProperty("四级单位")
     private String centralStat;
-
-    /**
-     * 组织单位拼接
-     * @return
-     */
-    public String getOrgName(){
-        StringBuilder sb = new StringBuilder();
-        if (this.getOrganizationId().length()==3){
-            return centralStat;
-        }
-        if (!StringUtils.isEmpty(this.getCompany())){
-            sb.append(this.getCompany()).append("/");
-        }
-        if (!StringUtils.isEmpty(this.getOilpro())){
-            sb.append(this.getOilpro()).append("/");
-        }
-        if (!StringUtils.isEmpty(this.getOpeArea())){
-            sb.append(this.getOpeArea()).append("/");
-        }
-        if (!StringUtils.isEmpty(this.getCentralStat())){
-            sb.append(this.getCentralStat()).append("/");
-        }
-        if (sb.length() > 0) {
-            sb.deleteCharAt(sb.length() - 1);
-        }
-        return sb.toString();
-
-    }
 
 
 }
