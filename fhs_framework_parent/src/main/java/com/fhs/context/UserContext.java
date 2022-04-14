ed909eca8d486c7c0b0d81bff640039fddeb4dbb
wanglei <wanglei@xhb.com>
2022-04-14 09:07:38 +0800

    添加获取groupcode的方法

--- fhs_framework_parent/src/main/java/com/fhs/context/UserContext.java
+++ fhs_framework_parent/src/main/java/com/fhs/context/UserContext.java
@@ -7,9 +7,28 @@ import org.springframework.web.context.request.ServletRequestAttributes;
 
 import javax.servlet.http.HttpServletRequest;
 
+/**
+ * 用户上下文
+ */
 public class UserContext {
+    /**
+     * 获取当前登录用户
+     * @return
+     */
     public static SysUserVo getSessionuser() {
         HttpServletRequest request = ((ServletRequestAttributes) RequestContextHolder.getRequestAttributes()).getRequest();
         return (SysUserVo) request.getSession().getAttribute(Constant.SESSION_USER);
+    }
+
+    /**
+     * 获取当前登录用户 group code
+     * @return
+     */
+    public static String getUserGroupCode() {
+        SysUserVo user = getSessionuser();
+        if (user != null) {
+            return user.getGroupCode();
+        }
+        return null;
     }
 }
