11ad82d51ba1443531495832307aa3aac540477b
wanglei <wanglei@xhb.com>
2022-04-20 14:54:53 +0800

    添加mybatis plus分页

---
+++ fhs_core/src/main/java/com/fhs/core/page/FhsPager.java
@@ -0,0 +1,88 @@
+package com.fhs.core.page;
+
+import com.alibaba.fastjson.annotation.JSONField;
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 用来分页使用
+ *
+ * @param <T>
+ */
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class FhsPager<T> implements IPage<T> {
+
+    private List<T> records;
+
+    private long total;
+
+    private long size;
+
+    private long current;
+
+
+    private long pageSize;
+
+    private long pageNumber;
+
+    private Map<String, Object> extMap;
+
+
+    @Override
+    public List<T> getRecords() {
+        return records;
+    }
+
+    @Override
+    public IPage<T> setRecords(List<T> records) {
+        this.records = records;
+        return this;
+    }
+
+    @Override
+    public IPage<T> setTotal(long total) {
+        this.total = total;
+        return this;
+    }
+
+    public void setPageSize(long pageSize) {
+        this.pageSize = pageSize;
+        this.size = pageSize;
+    }
+
+    public void setPageNumber(long pageNumber){
+        this.current = pageNumber;
+        this.pageNumber = pageNumber;
+    }
+
+    @Override
+    @JSONField(serialize = false)
+    public IPage<T> setSize(long size) {
+        this.size = size;
+        this.pageSize = size;
+        return this;
+    }
+
+    @Override
+    public long getCurrent() {
+        return current;
+    }
+
+    @Override
+    public IPage<T> setCurrent(long current) {
+        this.current = current;
+        return this;
+    }
+
+}
---
+++ fhs_framework_parent/src/main/java/com/fhs/config/MyabtisPlusPluginsConfig.java
@@ -0,0 +1,26 @@
+package com.fhs.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.pagination.dialects.MySqlDialect;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+
+@Configuration
+@EnableTransactionManagement
+public class MyabtisPlusPluginsConfig {
+
+    /**
+     * 新的分页插件,一缓和二缓遵循mybatis的规则,需要设置 MybatisConfiguration#useDeprecatedExecutor = false 避免缓存出现问题(该属性会在旧插件移除后一同移除)
+     */
+    @Bean
+    public PaginationInterceptor mybatisPlusInterceptor() {
+        PaginationInterceptor interceptor = new PaginationInterceptor();
+        interceptor.setDialectType(DbType.MYSQL.getDb());
+        interceptor.setDialectClazz(MySqlDialect.class.getName());
+        return interceptor;
+    }
+
+}
