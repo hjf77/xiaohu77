ab3c2fe4ac40caa38568e7a0cb17a6f52a1504e0
miyaxin <miyaxin0422@163.com>
2022-04-26 11:52:34 +0800

    添加微服务之间调用之间CloudApi

--- fhs_module/fhs_basics/fhs_basics_api/src/main/java/com/fhs/basics/service/LogOperationService.java
+++ fhs_module/fhs_basics/fhs_basics_api/src/main/java/com/fhs/basics/service/LogOperationService.java
@@ -3,6 +3,8 @@ package com.fhs.basics.service;
 import com.fhs.basics.po.LogOperationPO;
 import com.fhs.basics.vo.LogOperationVO;
 import com.fhs.core.base.service.BaseService;
+import com.fhs.easycloud.anno.CloudApi;
+import com.fhs.easycloud.anno.CloudMethod;
 
 /**
  * (LogOperation)}表服务接口
@@ -10,8 +12,11 @@ import com.fhs.core.base.service.BaseService;
  * @author chenzhihu
  * @since 2022-03-10 16:43:43
  */
+@CloudApi(serviceName = "basic", primary = false)
 public interface LogOperationService extends BaseService<LogOperationVO, LogOperationPO>{
 
-    
+    @CloudMethod
+    int insertSelective(LogOperationPO logOperationPO);
+
 
 }
