71804220d6358ee9d5e14e9c215b7eea98f85fed
LiangXiaoTao <905332966@qq.com>
2022-06-01 17:37:25 +0800

    供应商地理位置传参修改

--- fhs_app/fhs_app_all_in_one/src/main/java/com/fhs/supplier/vo/SupplierSupplierVO.java
+++ fhs_app/fhs_app_all_in_one/src/main/java/com/fhs/supplier/vo/SupplierSupplierVO.java
@@ -4,6 +4,7 @@ package com.fhs.supplier.vo;
 import com.fhs.supplier.po.SupplierSupplierPO;
 import com.fhs.core.trans.vo.VO;
 import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 /**
@@ -18,6 +19,26 @@ import lombok.*;
 @EqualsAndHashCode(callSuper = true)
 @ApiModel(value ="供应商",description ="供应商")
 public class SupplierSupplierVO extends SupplierSupplierPO implements VO {
-    
+
+ @ApiModelProperty(value = "地址")
+ private Integer[]geographicalPosition;
+
+ public Integer[] getGeographicalPosition() {
+  this.geographicalPosition = new Integer[3];
+  this.geographicalPosition[0] = this.getProvince();
+  this.geographicalPosition[1] = this.getCity();
+  this.geographicalPosition[2] = this.getArea();
+  return geographicalPosition;
  }
+
+ public void setGeographicalPosition(Integer[] geographicalPosition) {
+  if(geographicalPosition == null || geographicalPosition.length == 0){
+   return;
+  }
+  this.geographicalPosition = geographicalPosition;
+  this.setProvince(this.geographicalPosition[0]);
+  this.setCity(this.geographicalPosition[1]);
+  this.setArea(this.geographicalPosition[2]);
+ }
+}
     
