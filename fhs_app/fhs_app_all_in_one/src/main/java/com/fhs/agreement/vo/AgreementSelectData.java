5653732f1f326c5eba4d134b06edbe685d2da2e0
wanglei <wanglei@xhb.com>
2022-06-06 10:31:58 +0800

    添加对账条件代码

--- fhs_app/fhs_app_all_in_one/src/main/java/com/fhs/agreement/vo/AgreementSelectData.java
+++ fhs_app/fhs_app_all_in_one/src/main/java/com/fhs/agreement/vo/AgreementSelectData.java
@@ -1,12 +1,16 @@
 package com.fhs.agreement.vo;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import java.util.List;
 
 @Data
+@ApiModel("编辑的时候数据下拉数据回显")
 public class AgreementSelectData {
 
-    private List<Goodspecification> goodspecifications;
+    @ApiModelProperty("商品规格")
+    private List<Goodspecification> specificationId;
 
     @Data
     public static class Goodspecification{
