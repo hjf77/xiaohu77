b26cf36ac497cf3a4e40733bd3329cb0dec0c3bf
LiangXiaoTao <905332966@qq.com>
2022-06-06 11:18:58 +0800

    供应商付款条件代码生成

---
+++ fhs_app/fhs_app_all_in_one/src/main/java/com/fhs/supplier/service/SupplierPaymentConditionsService.java
@@ -0,0 +1,17 @@
+package com.fhs.supplier.service;
+
+import com.fhs.supplier.po.SupplierPaymentConditionsPO;
+import com.fhs.supplier.vo.SupplierPaymentConditionsVO;
+import com.fhs.core.base.service.BaseService;
+
+/**
+ * 供应商付款条件(SupplierPaymentConditions)}表服务接口
+ *
+ * @author liangxiaotao
+ * @since 2022-06-06 11:09:09
+ */
+public interface SupplierPaymentConditionsService extends BaseService<SupplierPaymentConditionsVO,SupplierPaymentConditionsPO>{
+
+    
+
+}
