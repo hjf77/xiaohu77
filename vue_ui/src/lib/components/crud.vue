aa901bedc06d093c9149e814112fb3f367565010
马军伟 <476810650@qq.com>
2022-06-09 11:31:05 +0800

    修改crud 删除 清空搜索参数问题 添加eventbus 刷新列表插件

--- vue_ui/src/lib/components/crud.vue
+++ vue_ui/src/lib/components/crud.vue
@@ -559,7 +559,7 @@ export default {
             })
               .then((res) => {
                 if (res.code === 200) {
-                  this.reset();
+                  this.reload();
                   this.$message({
                     type: "success",
                     message: "删除成功!",
@@ -620,7 +620,7 @@ export default {
               })
                 .then((res) => {
                   if (res.state) {
-                    this.reset();
+                    this.reload();
                     this.$message({
                       type: "success",
                       message: "删除成功!",
