47c5540ea1449b6f9517b6ef8a435acfb54a18c5
SunBin <3387639116@qq.com>
2022-06-13 16:23:50 +0800

    修改：去除拦截大屏接口

--- fhs_module/fhs_module_base/src/main/java/com/fhs/module/base/config/SaTokenConfig.java
+++ fhs_module/fhs_module_base/src/main/java/com/fhs/module/base/config/SaTokenConfig.java
@@ -3,11 +3,12 @@ package com.fhs.module.base.config;
 import cn.dev33.satoken.interceptor.SaRouteInterceptor;
 import cn.dev33.satoken.router.SaRouter;
 import cn.dev33.satoken.stp.StpUtil;
-import cn.dev33.satoken.util.SaResult;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * sa token配置用于权限验证
@@ -19,9 +20,12 @@ public class SaTokenConfig  implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         // 注册Sa-Token的路由拦截器
+        // 去除拦截大屏接口
+        List<String> excludePathList = new ArrayList<>();
+        excludePathList.add("/ms/bizEstate/list");
         registry.addInterceptor(new SaRouteInterceptor((req, res, handler)->{
             // 根据路由划分模块，不同模块不同鉴权
             SaRouter.match("/ms/**", r -> StpUtil.checkLogin());
-        })).addPathPatterns("/ms/**");
+        })).addPathPatterns("/ms/**").excludePathPatterns(excludePathList);
     }
 }
