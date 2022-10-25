3356764c2aee205b9518b00f60c65b3a856c5be8
wanglei <921888199@qq.com>
2022-10-25 11:58:55 +0800

    添加多表拦截器

--- fhs_module/fhs_module_base/src/main/java/com/fhs/module/base/config/MyabtisPlusPluginsConfig.java
+++ fhs_module/fhs_module_base/src/main/java/com/fhs/module/base/config/MyabtisPlusPluginsConfig.java
@@ -1,11 +1,31 @@
 package com.fhs.module.base.config;
 
 import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.annotation.TableName;
 import com.baomidou.mybatisplus.autoconfigure.ConfigurationCustomizer;
+import com.baomidou.mybatisplus.core.conditions.AbstractJoinWrapper;
+import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
+import com.baomidou.mybatisplus.core.toolkit.CollectionUtils;
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.InnerInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.JoinInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.apache.ibatis.executor.Executor;
+import org.apache.ibatis.mapping.BoundSql;
+import org.apache.ibatis.mapping.MappedStatement;
+import org.apache.ibatis.mapping.ResultMap;
+import org.apache.ibatis.session.ResultHandler;
+import org.apache.ibatis.session.RowBounds;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Configuration
 public class MyabtisPlusPluginsConfig {
@@ -15,8 +35,35 @@ public class MyabtisPlusPluginsConfig {
     @Bean
     public MybatisPlusInterceptor mybatisPlusInterceptor() {
         MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
-        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.POSTGRE_SQL));
+        interceptor.addInnerInterceptor(new FhsJoinInterceptor());
         return interceptor;
+    }
+
+
+}
+
+/**
+ * 解决MP4 版本中的mybatis 异常问题
+ */
+class FhsJoinInterceptor extends JoinInterceptor {
+    /**
+     * 缓存MappedStatement,不需要每次都去重写构建MappedStatement
+     */
+    private static final Map<String, Map<org.apache.ibatis.session.Configuration, MappedStatement>> MS_CACHE = new ConcurrentHashMap<>();
+
+    @Override
+    public MappedStatement changeMappedStatement(Executor executor, MappedStatement ms, Object parameter, RowBounds rowBounds, ResultHandler resultHandler, BoundSql boundSql) throws SQLException {
+        if (parameter != null && parameter instanceof Map) {
+            if (((Map) parameter).containsKey("ew")) {
+                Object ew = ((Map) parameter).get("ew");
+                if (ew != null && ew instanceof AbstractJoinWrapper && ms.getResultMaps() != null && !ms.getResultMaps().isEmpty()) {
+                    return this.newMappedStatement(ms, ((ResultMap) ms.getResultMaps().get(0)).getType());
+                }
+            }
+        }
+        return ms;
+
     }
 
 
