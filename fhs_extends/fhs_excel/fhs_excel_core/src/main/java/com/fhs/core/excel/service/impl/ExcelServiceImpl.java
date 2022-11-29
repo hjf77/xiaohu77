183e9c237589a8bb4aa1698fcc1c0edddd7c729b
LiangXiaoTao <905332966@qq.com>
2022-11-29 14:50:43 +0800

    导入反翻译

--- fhs_extends/fhs_excel/fhs_excel_core/src/main/java/com/fhs/core/excel/service/impl/ExcelServiceImpl.java
+++ fhs_extends/fhs_excel/fhs_excel_core/src/main/java/com/fhs/core/excel/service/impl/ExcelServiceImpl.java
@@ -383,7 +383,8 @@ public class ExcelServiceImpl implements ExcelService {
                                     String targetFieldName = targetField.getName();
                                     ApiModelProperty apiModelProperty = targetField.getAnnotation(ApiModelProperty.class);
                                     //获取到导入翻译对应的字段
-                                    if (transFields.contains(targetFieldName) && apiModelProperty != null && fieldName.equals(apiModelProperty.value())) {
+                                    ImportExcelTitle importExcelTitle = field.getAnnotation(ImportExcelTitle.class);
+                                    if (transFields.contains(targetFieldName) && apiModelProperty != null && (fieldName.equals(apiModelProperty.value()) || apiModelProperty.value().equals(importExcelTitle.value()) )) {
                                         //获取当前target的service
                                         BaseService baseService = SpringContextUtil.getBeanByClass(BaseService.class, target.getName(), 1);
                                         QueryWrapper queryWrapper = new QueryWrapper();
