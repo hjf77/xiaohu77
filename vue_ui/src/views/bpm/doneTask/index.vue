83005b488073554aa11c503d65928a6947cf3102
cym <2935609537@qq.com>
2022-12-02 14:25:29 +0800

    新增待办任务、已办任务页面

---
+++ vue_ui/src/views/bpm/doneTask/index.vue
@@ -0,0 +1,107 @@
+<!--
+  模块名称：已办任务
+  开发人员：chuyemiao
+  创建时间:2022/12/2 
+-->
+<template>
+    <div class="app-container">
+      <pagex-crud
+        :filters="filters"
+        :columns="columns"
+        :api="api"
+        :buttons="buttons"
+        :methodType="methodType"
+        :namespace="namespace"
+      >
+        <template v-slot:form="prop">
+          <!-- 修改 弹框-->
+          <pagex-dialog slot="form"
+           :title="title"  
+           :visible.sync="open" 
+           :before-close="closeFn" 
+            class="pagex-dialog-theme">
+            <!-- <editForm v-if="isEdit" :init="init" 
+            :isEdit="isEdit">编辑</editForm>
+            <addForm v-else :isAdd="isAdd">新增</addForm> -->
+          </pagex-dialog>
+        </template>
+      </pagex-crud>
+    </div>
+  </template>
+  <script>
+  export default {
+    name: "done",
+    components: {
+    },
+    data() {
+      return {
+        namespace: 'done',
+        api: '/basic/ms/task/done-page',
+        methodType: 'post',
+        ruleTitle: '任务分配规则',
+        isEdit: false,
+        isAdd: false,
+        init: {},
+        ruleId: '',
+        buttons: [
+        ],
+        columns: [
+          {label: '任务编号', name: 'id',width:'300'},
+          {label: '任务名称', name: 'name',width:'160'},
+          {label: '所属流程', name: 'processInstance.name',width:'150'},
+          {label: '流程发起人', name: 'processInstance.startUserNickname',width:'150' },
+          {label: '结果', name: 'transMap.resultName',type:'formart', 
+          formart: function(row) {
+            let color = "#13ce66";
+            if (row.result == 2) {
+              color = "#09DD84";
+            }
+            if (row.result == 4) {
+              color = "#ff4949";
+            }
+            return ( "<div style='color:" + color + ";'>" + row.transMap.resultName + "</div>" );
+          },},
+          {label: '审批意见', name: 'reason', type: '',width:'200'}, 
+          {label: '创建时间', name: 'createTime',width:'150'},
+          {label: '审批时间', name: 'endTime',width:'150'},
+          {label: '耗时', name: 'durationInMillis',
+          },
+          {
+            label: '操作',
+            name: 'operation',
+            type: 'textBtn',
+            width: '100px',
+            textBtn: [
+            {
+              title: "详情",
+              icon: 'el-icon-edit',
+              type: "text",
+              size: 'mini',
+              click: (_row) => {
+                // this.$set(this, "init", _row);
+                this.$router.push({
+                  path: '/bpm/processDetail',
+                  query:{id: _row.id}
+                });
+              }
+            },
+          ],
+          }
+        ],
+        filters: [
+        {label: '流程名', name: 'name', type: 'text'},
+        {label: '创建时间', name: 'createTime', type: 'date-picker',},
+      ]
+      };
+    },
+    created() {
+     
+    },
+    methods: {
+      closeFn(){
+        this.open = true;
+      },
+    }
+  };
+  </script>
+  
