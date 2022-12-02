4f822ddf0c9350fcf28fe3a9c10d5a72c8048590
tanyukun <tan_yukun@outlook.com>
2022-12-02 18:34:43 +0800

    解决冲突

--- fhs_extends/fhs_base/fhs_base_pojo/src/main/java/com/fhs/core/base/vo/QueryFilter.java
+++ fhs_extends/fhs_base/fhs_base_pojo/src/main/java/com/fhs/core/base/vo/QueryFilter.java
@@ -1,35 +1,11 @@
 package com.fhs.core.base.vo;
 
-import com.alibaba.fastjson.annotation.JSONField;
-import com.baomidou.mybatisplus.annotation.TableField;
-import com.baomidou.mybatisplus.annotation.TableId;
-import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
-import com.baomidou.mybatisplus.core.metadata.TableInfoHelper;
-import com.baomidou.mybatisplus.core.toolkit.LambdaUtils;
-import com.baomidou.mybatisplus.core.toolkit.support.ColumnCache;
-import com.baomidou.mybatisplus.extension.activerecord.Model;
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import com.fhs.common.utils.*;
-import com.fhs.core.base.anno.SafeField;
-import com.github.liangbaika.validate.exception.ParamsInValidException;
+import com.baomidou.mybatisplus.advance.query.QueryField;
 import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.ibatis.annotations.Param;
-import org.springframework.util.Assert;
-import org.springframework.util.ReflectionUtils;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import javax.servlet.http.HttpServletRequest;
-import java.lang.reflect.Field;
 import java.util.*;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 import java.util.stream.Collectors;
 
 @Data
@@ -37,5 +13,16 @@ import java.util.stream.Collectors;
 @AllArgsConstructor
 @ApiModel("用于自定义条件过滤")
 public class QueryFilter<T> extends com.baomidou.mybatisplus.advance.query.QueryFilter<T> {
+    public Map<String, Object> queryFieldsMap() {
 
+        Map<String, Object> paramParams = new HashMap<>();
+        if (null != super.getQuerys()) {
+            paramParams = super.getQuerys().stream().collect(Collectors.toMap(QueryField::getProperty, QueryField::getValue));
+        }
+        if (null != super.getPagerInfo()) {
+            paramParams.put("pageNumber", super.getPagerInfo().getPageNumber());
+            paramParams.put("pageSize", super.getPagerInfo().getPageSize());
+        }
+        return paramParams;
+    }
 }
